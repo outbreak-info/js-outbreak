d2e0ae99733c9ce14f2c30dc21c2c4adbfc9653f
rrouz <ryinrouz94@gmail.com>
2025-03-24 16:47:00 -0700

    Added ScatterChart to index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 // Import components
 import BarChart from './components/BarChart.vue'
+import ScatterChart from './components/ScatterChart.vue'
 
 // Export components
 export {
-    BarChart
+    BarChart,
+    ScatterChart
 }
 
 // Export default object for Vue.use()
 export default {
     install: (app: any) => {
         app.component('BarChart', BarChart)
+        app.component('ScatterChart', ScatterChart)
     }
 }
