57ce6f53ff8cb200e4fe1e8fc2e11d5782852a7a
rrouz <ryinrouz94@gmail.com>
2025-04-03 19:01:13 -0700

    Update TestHistogramChart.vue to use new props for HistogramChart

--- tests/visual/components/TestHistogramChart.vue
+++ tests/visual/components/TestHistogramChart.vue
@@ -5,7 +5,9 @@
     <div class="chart-wrapper">
       <HistogramChart
         :data=testData
-        :bins="[0,1,2,3,4,5,6,7,8,9]"
+        :xMin="0"
+        :xMax="1"
+        :binCount="10"
       />
     </div>
   </div>
@@ -17,13 +19,17 @@ import HistogramChart from '../../../src/components/HistogramChart.vue'
 import { colorPalette } from '../../../src/utils/colorSchemes'
 
 const testData = ref([
-  { id: 1, frequency: 5 },
-  { id: 2, frequency: 5 },
-  { id: 3, frequency: 4 },
-  { id: 4, frequency: 6 },
-  { id: 5, frequency: 5 },
-  { id: 6, frequency: 4 },
-  { id: 7, frequency: 3 }
+  { id: 1, frequency: 0.1 },
+  { id: 2, frequency: 0.2 },
+  { id: 3, frequency: 0.2 },
+  { id: 4, frequency: 0.3 },
+  { id: 5, frequency: 0.3 },
+  { id: 6, frequency: 0.3 },
+  { id: 7, frequency: 0.4 },
+  { id: 8, frequency: 0.5 },
+  { id: 9, frequency: 0.6 },
+  { id: 10, frequency: 0.7 },
+  { id: 11, frequency: 0.8 },
+  { id: 12, frequency: 0.9 }
 ]);
-
 </script>
