cb3c83ca922b13ece0b62dbcf2463c91461aca2d
gkarthik <xraydiffraction@gmail.com>
2025-07-04 16:58:20 -0700

    feat: Added a loading spinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,18 @@
+<template>
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-spin size="medium" :description="props.description" />
+  </n-config-provider>
+</template>
+
+<script setup lang="ts">
+import { NSpin, NConfigProvider } from 'naive-ui'
+import { themeOverrides } from "../assets/naiveThemeVariables"
+
+const props = defineProps({
+  description: { type: String, default: 'Loading ...' },
+});
+
+</script>
+
+<style scoped>
+</style>
---
+++ tests/visual/components/TestLoadingSpinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test-container">
+    <h1>Loading Spinner Test</h1>
+
+    <h2>Default Loading</h2>
+    <LoadingSpinner />
+
+    <h2>Custom  Description</h2>
+    <LoadingSpinner description="Loading data..." />
+  </div>
+</template>
+
+<script setup lang="ts">
+import LoadingSpinner from '../../../src/components/LoadingSpinner.vue'
+</script>
+
