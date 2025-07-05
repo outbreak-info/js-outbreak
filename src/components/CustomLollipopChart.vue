466a8c7d45cabac3350be57a4ae1cb3d5e5539ce
Leandro Collares <leandrocollares@users.noreply.github.com>
2025-07-05 11:32:36 -0300

    feat: improve chart responsiveness (#9)

--- src/components/CustomLollipopChart.vue
+++ src/components/CustomLollipopChart.vue
@@ -1,9 +1,170 @@
+<script setup>
+import { ref, computed, onMounted, onUnmounted } from "vue";
+import { scaleLinear, scaleBand } from "d3-scale";
+import { max } from "d3-array";
+import { format } from "d3-format";
+import { timeFormat, timeParse } from "d3-time-format";
+import { quadtree } from "d3-quadtree";
+import { createDateArray } from "../utils/arrays";
+
+const props = defineProps({
+  data: { type: Array, required: true },
+  dateKey: { type: String, default: "key" },
+  valueKey: { type: String, default: "val" },
+  xAxisLabel: { type: String, default: "Date" },
+  yAxisLabel: { type: String, default: "Value" },
+  height: { type: Number, default: 135 },
+
+  // Margins
+  marginTop: { type: Number, default: 35 },
+  marginRight: { type: Number, default: 130 },
+  marginBottom: { type: Number, default: 50 },
+  marginLeft: { type: Number, default: 40 },
+
+  // Container margins
+  containerMarginTop: { type: Number, default: 0 },
+  containerMarginRight: { type: Number, default: 10 },
+  containerMarginBottom: { type: Number, default: 0 },
+  containerMarginLeft: { type: Number, default: 10 },
+});
+
+const width = ref(500);
+const hoveredPoint = ref(null);
+
+const containerMargins = computed(() => ({
+  marginTop: props.containerMarginTop + "px",
+  marginRight: props.containerMarginRight + "px",
+  marginBottom: props.containerMarginBottom + "px",
+  marginLeft: props.containerMarginLeft + "px",
+}));
+
+onMounted(() => {
+  window.addEventListener("resize", handleResize);
+  handleResize();
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+
+const handleResize = () => {
+  if (window.innerWidth >= 1000) {
+    width.value = 1000;
+  } else {
+    width.value = window.innerWidth;
+  }
+};
+
+const xAccessor = (d) => d[props.dateKey];
+const yAccessor = (d) => d[props.valueKey];
+
+const formatHoveredValueKey = format(",.0f");
+const formatValueKey = format(".2s");
+const parseTime = timeParse("%Y-%m-%d");
+const formatTime = timeFormat("%b %e");
+
+const responsivePointRadius = computed(() => {
+  if (width.value < 400) return 2;
+  if (width.value < 600) return 3;
+  return 4;
+});
+
+const responsiveStrokeWidth = computed(() => {
+  if (width.value < 400) return "1px";
+  return "2px";
+});
+
+const marginTop = props.marginTop;
+const marginRight = props.marginRight;
+const marginBottom = props.marginBottom;
+const marginLeft = props.marginLeft;
+
+const innerWidth = computed(() => width.value - marginLeft - marginRight);
+const innerHeight = computed(() => props.height - marginTop - marginBottom);
+
+const xScaleDomain = computed(() =>
+  createDateArray(
+    xAccessor(props.data[0]),
+    xAccessor(props.data[props.data.length - 1])
+  )
+);
+
+const xScale = computed(() =>
+  scaleBand().domain(xScaleDomain.value).range([0, innerWidth.value])
+);
+
+const yScale = computed(() =>
+  scaleLinear()
+    .domain([0, max(props.data, yAccessor)])
+    .range([innerHeight.value, 0])
+    .nice()
+);
+
+const xAccessorScaled = computed(() => (d) => xScale.value(xAccessor(d)));
+const yAccessorScaled = computed(() => (d) => yScale.value(yAccessor(d)));
+
+const quadtreeInstance = computed(() =>
+  quadtree()
+    .x((d) => xScale.value(xAccessor(d)))
+    .y((d) => yScale.value(yAccessor(d)))
+    .addAll(props.data)
+);
+
+const yTicks = computed(() => {
+  const numberOfYTicks = Math.floor(innerHeight.value / 40);
+  return yScale.value.ticks(numberOfYTicks);
+});
+
+const allXTicks = computed(() => xScale.value.domain());
+
+const filterXTicks = (numberOfXTicks, width) => {
+  if (numberOfXTicks > 270) {
+    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 60));
+    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 90));
+    else return allXTicks.value.filter((d, i) => !(i % 210));
+  }
+  if (numberOfXTicks > 210) {
+    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 30));
+    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 60));
+    else return allXTicks.value.filter((d, i) => !(i % 90));
+  }
+  if (numberOfXTicks > 120) {
+    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 21));
+    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 30));
+    else return allXTicks.value.filter((d, i) => !(i % 60));
+  } else {
+    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 14));
+    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 21));
+    else if (width > 400) return allXTicks.value.filter((d, i) => !(i % 30));
+    else return allXTicks.value.filter((d, i) => !(i % 45));
+  }
+};
+
+const xTicksToBeRendered = computed(() =>
+  filterXTicks(xScaleDomain.value.length, innerWidth.value)
+);
+
+const handleMouseMove = (e) => {
+  const xPosition = e.offsetX - marginLeft;
+  const yPosition = e.offsetY - marginTop;
+  const foundPoint = quadtreeInstance.value.find(xPosition, yPosition);
+
+  if (foundPoint) {
+    hoveredPoint.value = foundPoint;
+  }
+};
+
+const handleMouseLeave = () => {
+  hoveredPoint.value = null;
+};
+</script>
+
 <template>
-  <div class="chart-container">
+  <div class="chart-container" :style="containerMargins">
     <svg
       role="img"
       :aria-label="`Lollipop chart showing ${yAxisLabel} over time`"
-      :width="chartWidth"
+      :width="width - containerMarginLeft - containerMarginRight"
       :height="height"
       @mousemove="handleMouseMove"
       @mouseleave="handleMouseLeave"
@@ -25,6 +186,7 @@
             text-anchor="middle"
             fill="#2c3e50"
             font-size="14px"
+            font-weight="700"
           >
             {{ yAxisLabel }}
           </text>
@@ -55,15 +217,14 @@
             y="45"
             fill="#2c3e50"
             font-size="14px"
+            font-weight="700"
           >
             {{ xAxisLabel }}
           </text>
           <g
             v-for="(tick, index) in xTicksToBeRendered"
             :key="'tick-' + index"
-            :transform="`translate(${
-              xScale(tick) + xScale.bandwidth() / 2
-            }, 0)`"
+            :transform="`translate(${xScale(tick)}, 0)`"
           >
             <line :y1="0" :y2="6" stroke="#bdc3c7" />
             <text
@@ -159,179 +320,8 @@
   </div>
 </template>
 
-<script setup>
-import { ref, computed, onMounted, onUnmounted } from "vue";
-import { scaleLinear, scaleBand } from "d3-scale";
-import { max } from "d3-array";
-import { format } from "d3-format";
-import { timeFormat, timeParse } from "d3-time-format";
-import { quadtree } from "d3-quadtree";
-import { createDateArray } from "../utils/arrays";
-
-const props = defineProps({
-  data: { type: Array, required: true },
-  dateKey: { type: String, default: "key" },
-  valueKey: { type: String, default: "val" },
-  xAxisLabel: { type: String, default: "Date"},
-  yAxisLabel: { type: String, default: "Value"},
-
-  // Margins
-  marginTop: { type: Number, default: 35 },
-  marginRight: { type: Number, default: 25 },
-  marginBottom: { type: Number, default: 50 },
-  marginLeft: { type: Number, default: 50 },
-
-  // Container margins
-  containerLeftMargin: { type: Number, default: 50 },
-  containerRightMargin: { type: Number, default: 50 },
-});
-
-const windowWidth = ref(500);
-const hoveredPoint = ref(null);
-const height = 155;
-
-const chartWidth = computed(() => {
-  if (windowWidth.value >= 1000) {
-    return 1000 - props.containerLeftMargin - props.containerRightMargin;
-  } else {
-    return (
-      windowWidth.value - props.containerLeftMargin - props.containerRightMargin
-    );
-  }
-});
-
-const xAccessor = (d) => d[props.dateKey];
-const yAccessor = (d) => d[props.valueKey];
-
-const formatHoveredValueKey = format(",.0f");
-const formatValueKey = format(".2s");
-const parseTime = timeParse("%Y-%m-%d");
-const formatTime = timeFormat("%b %e");
-
-const responsivePointRadius = computed(() => {
-  if (chartWidth.value < 400) return 2;
-  if (chartWidth.value < 600) return 3;
-  return 4;
-});
-
-const responsiveStrokeWidth = computed(() => {
-  if (chartWidth.value < 400) return "1px";
-  return "2px";
-});
-
-const marginTop = props.marginTop;
-const marginRight = props.marginRight;
-const marginBottom = props.marginBottom;
-const marginLeft = props.marginLeft;
-
-const innerWidth = computed(() => chartWidth.value - marginLeft - marginRight);
-const innerHeight = height - marginTop - marginBottom;
-
-const xScaleDomain = computed(() =>
-  createDateArray(
-    xAccessor(props.data[0]),
-    xAccessor(props.data[props.data.length - 1])
-  )
-);
-
-const xScale = computed(() =>
-  scaleBand().domain(xScaleDomain.value).range([0, innerWidth.value])
-);
-
-const yScale = computed(() =>
-  scaleLinear()
-    .domain([0, max(props.data, yAccessor)])
-    .range([innerHeight, 0])
-    .nice()
-);
-
-const xAccessorScaled = computed(
-  () => (d) => xScale.value(xAccessor(d)) + xScale.value.bandwidth() / 2
-);
-const yAccessorScaled = computed(() => (d) => yScale.value(yAccessor(d)));
-
-const quadtreeInstance = computed(() =>
-  quadtree()
-    .x((d) => xScale.value(xAccessor(d)) + xScale.value.bandwidth() / 2)
-    .y((d) => yScale.value(yAccessor(d)))
-    .addAll(props.data)
-);
-
-const yTicks = computed(() => {
-  const numberOfYTicks = Math.floor(innerHeight / 40);
-  return yScale.value.ticks(numberOfYTicks);
-});
-
-const allXTicks = computed(() => xScale.value.domain());
-
-const filterXTicks = (numberOfXTicks, width) => {
-  if (numberOfXTicks > 270) {
-    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 60));
-    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 90));
-    else return allXTicks.value.filter((d, i) => !(i % 210));
-  }
-  if (numberOfXTicks > 210) {
-    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 30));
-    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 60));
-    else return allXTicks.value.filter((d, i) => !(i % 90));
-  }
-  if (numberOfXTicks > 120) {
-    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 21));
-    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 30));
-    else return allXTicks.value.filter((d, i) => !(i % 60));
-  } else {
-    if (width > 700) return allXTicks.value.filter((d, i) => !(i % 14));
-    else if (width > 550) return allXTicks.value.filter((d, i) => !(i % 21));
-    else if (width > 400) return allXTicks.value.filter((d, i) => !(i % 30));
-    else return allXTicks.value.filter((d, i) => !(i % 45));
-  }
-};
-
-const xTicksToBeRendered = computed(() =>
-  filterXTicks(xScaleDomain.value.length, innerWidth.value)
-);
-
-const handleResize = () => {
-  if (window.innerWidth >= 1000) {
-    windowWidth.value = 1000;
-  } else {
-    windowWidth.value = window.innerWidth;
-  }
-};
-
-const handleMouseMove = (e) => {
-  const xPosition = e.offsetX - marginLeft;
-  const yPosition = e.offsetY - marginTop;
-  const foundPoint = quadtreeInstance.value.find(xPosition, yPosition);
-
-  if (foundPoint) {
-    hoveredPoint.value = foundPoint;
-  }
-};
-
-const handleMouseLeave = () => {
-  hoveredPoint.value = null;
-};
-
-onMounted(() => {
-  window.addEventListener("resize", handleResize);
-  handleResize();
-});
-
-onUnmounted(() => {
-  window.removeEventListener("resize", handleResize);
-});
-</script>
-
 <style scoped>
 .chart-container {
   position: relative;
-  display: flex;
-  flex-direction: column;
-  max-width: 1000px;
-  width: 100%;
-  text-align: left;
-  margin-top: 15px;
-  margin-bottom: 20px;
 }
 </style>
