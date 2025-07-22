6a9372d1aeb9b10074a99b29b64f9899ff02de75
Leandro Collares <leandrocollares@users.noreply.github.com>
2025-07-22 19:43:40 -0300

    feature/no-ref/custom-us-choropleth-map-improvements (#12)
    * feat: add legend
    
    * chore: use inline styles to cutomize legend
    
    * chore: move hatch pattern to different file
    
    * feat: use prop for string associated with hatch pattern
    
    * feat: improve tooltip positioning
    
    * feat: add more tooltip customization possibilities

--- src/components/CustomUsChoroplethMap.vue
+++ src/components/CustomUsChoroplethMap.vue
@@ -1,14 +1,40 @@
 <script setup>
 import { ref, computed, onMounted, onUnmounted } from "vue";
 import { geoAlbersUsa, geoPath } from "d3-geo";
-import { scaleThreshold } from "d3-scale";
-import { schemeYlGnBu } from "d3-scale-chromatic";
+import { scaleThreshold, scaleLinear } from "d3-scale";
+import {
+  ylGnBuDiscrete11,
+  diagonalHatchPatternDef,
+} from "../utils/colorSchemes";
 import { format } from "d3-format";
+import { min, max } from "d3-array";
 import usGeoJson from "../assets/geo/us_states.json";
 
 const props = defineProps({
   data: { type: Array, required: true },
   valueKey: { type: String, default: "value" },
+  colorKey: { type: String, default: null }, // Defaults to valueKey if not provided
+  secondaryValue: { type: String },
+  hatchPatternString: { type: String, default: "no data" },
+
+  // Color scale configuration
+  colorDomain: {
+    type: Array,
+    default: () => [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  },
+  colorRange: { type: Array, default: () => ylGnBuDiscrete11 },
+
+  // Tooltip configuration
+  tooltipFields: {
+    type: Array,
+    default: () => [
+      { label: "Prevalence", key: "prevalence", formatter: ".2f", suffix: "%" },
+      { label: "Samples", key: "num_samples", formatter: ",.0f" },
+    ],
+  },
+
+  // Legend configuration
+  legendTitle: { type: String, default: "prevalence (%)" },
 
   // Container margins
   containerMarginTop: { type: Number, default: 0 },
@@ -30,6 +56,8 @@ const hoveredState = ref(null);
 const eventPosition = ref(null);
 const xPosition = ref(null);
 const yPosition = ref(null);
+
+const formatLegendValue = format(".2s");
 
 onMounted(() => {
   window.addEventListener("resize", handleResize);
@@ -61,35 +89,27 @@ const projection = computed(() =>
 
 const pathGenerator = computed(() => geoPath().projection(projection.value));
 
+const actualColorKey = computed(() => props.colorKey || props.valueKey);
+
 const colorScale = computed(() =>
-  scaleThreshold()
-    .domain([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
-    .range(schemeYlGnBu[9])
+  scaleThreshold().domain(props.colorDomain).range(props.colorRange)
 );
 
 // Create data lookup map
 const dataLookup = computed(() => {
   const lookup = {};
   props.data.forEach((item) => {
-    if (item.state && item[props.valueKey] !== undefined) {
-      lookup[item.state] = item[props.valueKey];
+    if (item.state) {
+      lookup[item.state] = item;
     }
   });
   return lookup;
 });
 
-const formatPrevalence = (stateName) => {
-  const value = dataLookup.value[stateName];
-  if (value !== undefined) {
-    return format(".2f")(value);
-  }
-  return "no data";
-};
-
 const getStateFill = (stateName) => {
-  const value = dataLookup.value[stateName];
-  if (value !== undefined) {
-    return colorScale.value(value);
+  const stateData = dataLookup.value[stateName];
+  if (stateData && stateData[actualColorKey.value] !== undefined) {
+    return colorScale.value(stateData[actualColorKey.value]);
   }
   return "url(#diagonalHatch)";
 };
@@ -108,10 +128,56 @@ const getStateStrokeWidth = (stateName) => {
   return 1;
 };
 
+const formatFieldValue = (value, formatter, suffix = "") => {
+  if (value === undefined || value === null) {
+    return props.hatchPatternString;
+  }
+
+  let formattedValue;
+
+  if (formatter) {
+    try {
+      formattedValue = format(formatter)(value);
+    } catch (e) {
+      formattedValue = value.toString();
+    }
+  } else {
+    formattedValue = value.toString();
+  }
+
+  return formattedValue + suffix;
+};
+
+const tooltipData = computed(() => {
+  if (!hoveredState.value) return [];
+
+  const stateData = dataLookup.value[hoveredState.value];
+  if (!stateData) return [];
+
+  return props.tooltipFields.map((field) => ({
+    label: field.label,
+    value: formatFieldValue(
+      stateData[field.key],
+      field.formatter,
+      field.suffix || ""
+    ),
+  }));
+});
+
 const handleMouseEnter = (event, feature) => {
   hoveredState.value = feature.properties.NAME;
   eventPosition.value = { x: event.pageX, y: event.pageY };
-  xPosition.value = eventPosition.value.x - 120;
+  if (width.value >= 800) {
+    xPosition.value =
+      eventPosition.value.x >= 400
+        ? eventPosition.value.x - 150
+        : eventPosition.value.x - 20;
+  } else {
+    xPosition.value =
+      eventPosition.value.x >= 250
+        ? eventPosition.value.x - 100
+        : eventPosition.value.x - 10;
+  }
   yPosition.value = eventPosition.value.y + 50;
 };
 
@@ -132,11 +198,64 @@ const hoveredStateFeature = computed(() =>
   )
 );
 
+const legendWidth = 300;
+const legendHeight = 45;
+const rectWidth = 25;
+const rectHeight = 15;
+
+const rangeMin = 12;
+const rangeMax = 288;
+
+const legendScale = scaleLinear()
+  .domain([min(colorScale.value.domain()), max(colorScale.value.domain())])
+  .rangeRound([rangeMin, rangeMax]);
+
+const colorBands = colorScale.value.range().map((color) => {
+  const d = colorScale.value.invertExtent(color);
+
+  if (d[0] == null) d[0] = legendScale.domain()[0];
+  if (d[1] == null) d[1] = legendScale.domain()[1];
+
+  return d;
+});
+
+const ticks = colorScale.value.domain();
+
+// Choropleth container inline styles
+const choroplethContainerStyle = computed(() => ({
+  position: "relative",
+  ...containerMargins.value,
+}));
+
+// Legend inline styles
+const legendWrapperStyle = {
+  display: "flex",
+  flexFlow: "row wrap",
+  alignItems: "center",
+  marginBottom: "0px",
+};
+
+const legendStyle = {
+  marginLeft: "0px",
+  marginRight: "0px",
+};
+
+const titleStyle = {
+  marginBottom: "5px",
+  textAlign: "left",
+  fontSize: "14px",
+  marginLeft: "10px",
+};
+
+const noDataStyle = {
+  marginLeft: "7px",
+};
+
 // Tooltip inline styles
 const tooltipWrapperStyle = computed(() => ({
   left: xPosition.value + "px",
   top: yPosition.value + "px",
-  width: "160px",
+  width: "140px",
   background: "#ffffff",
   boxShadow: "1px 2px 7px rgba(0, 0, 0, 0.2)",
   borderRadius: "3px",
@@ -170,38 +289,81 @@ const tooltipGridStyle = {
   marginBottom: "5px",
 };
 
-const tooltipBarStyle = computed(() => ({
-  position: "absolute",
-  height: "7px",
-  width: "100%",
-  bottom: "0",
-  left: "0",
-  background: hoveredState.value
-    ? colorScale.value(dataLookup.value[hoveredState.value])
-    : "transparent",
-}));
+const tooltipBarStyle = computed(() => {
+  const stateData = dataLookup.value[hoveredState.value];
+  const value = stateData ? stateData[actualColorKey.value] : null;
+
+  return {
+    position: "absolute",
+    height: "7px",
+    width: "100%",
+    bottom: "0",
+    left: "0",
+    background:
+      value !== null && value !== undefined
+        ? colorScale.value(value)
+        : "transparent",
+  };
+});
 </script>
 
 <template>
-  <div class="choropleth-container" :style="containerMargins">
-    <svg :width="renderedWidth" :height="renderedHeight">
-      <defs>
-        <pattern
-          id="diagonalHatch"
-          width="5"
-          height="5"
-          patternTransform="rotate(45 0 0)"
-          patternUnits="userSpaceOnUse"
-        >
-          <line
-            x1="0"
-            y1="0"
-            x2="0"
-            y2="10"
-            :style="`stroke:#a9a9a9; stroke-width:2`"
+  <div class="choropleth-container" :style="choroplethContainerStyle">
+    <!-- Legend -->
+    <div class="legend-wrapper" :style="legendWrapperStyle">
+      <div class="legend" :style="legendStyle">
+        <div class="title" :style="titleStyle">
+          <span>{{ legendTitle }}</span>
+        </div>
+        <svg role="img" :width="legendWidth" :height="legendHeight">
+          <defs v-html="diagonalHatchPatternDef('legendDiagonalHatch')"></defs>
+          <g>
+            <rect
+              v-for="band in colorBands"
+              :x="legendScale(band[0])"
+              :width="legendScale(band[1]) - legendScale(band[0])"
+              :height="rectHeight"
+              :fill="colorScale(band[0])"
+            />
+            <g
+              v-for="tick in ticks"
+              :transform="`translate(${legendScale(tick)}, 0)`"
+            >
+              <text
+                y="18"
+                dy="0.8em"
+                text-anchor="middle"
+                fill="#2c3e50"
+                font-size="14px"
+              >
+                {{ tick == 0 ? tick : formatLegendValue(tick) }}
+              </text>
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div class="no-data" :style="noDataStyle">
+        <svg width="125" height="24">
+          <defs v-html="diagonalHatchPatternDef('legendDiagonalHatch')"></defs>
+          <rect
+            x="5"
+            y="2"
+            :width="rectWidth"
+            :height="rectHeight"
+            fill="url(#legendDiagonalHatch)"
+            stroke="#888"
+            stroke-width="0.5"
           />
-        </pattern>
-      </defs>
+          <text x="40" y="12" dy="0.1em" fill="#2c3e50" font-size="14px">
+            {{ hatchPatternString }}
+          </text>
+        </svg>
+      </div>
+    </div>
+
+    <!-- Map -->
+    <svg :width="renderedWidth" :height="renderedHeight">
+      <defs v-html="diagonalHatchPatternDef('diagonalHatch')"></defs>
 
       <!-- Non-hovered states -->
       <path
@@ -230,6 +392,7 @@ const tooltipBarStyle = computed(() => ({
       />
     </svg>
   </div>
+
   <!-- Tooltip -->
   <div v-if="hoveredState && eventPosition" :style="tooltipWrapperStyle">
     <div :style="tooltipTitleStyle">
@@ -237,19 +400,13 @@ const tooltipBarStyle = computed(() => ({
       <hr :style="tooltipDividerStyle" />
     </div>
     <div :style="tooltipGridStyle">
-      <span>Prevalence (%)</span>
-      <span>
-        {{ formatPrevalence(hoveredState) }}
-      </span>
+      <template v-for="field in tooltipData" :key="field.label">
+        <span>{{ field.label }}</span>
+        <span>{{ field.value }}</span>
+      </template>
     </div>
     <div>
       <span :style="tooltipBarStyle" />
     </div>
   </div>
 </template>
-
-<style scoped>
-.choropleth-container {
-  position: relative;
-}
-</style>
--- src/utils/colorSchemes.ts
+++ src/utils/colorSchemes.ts
@@ -1,32 +1,68 @@
-import { schemeYlGnBu } from 'd3-scale-chromatic'
+import { schemeYlGnBu } from "d3-scale-chromatic";
 
 export const choroColorScheme = schemeYlGnBu;
 export const heatmapColorScheme = "RdPu";
 
-export const defaultColor = '#3498db';
+export const defaultColor = "#3498db";
 
-export const otherColor = '#bab0ab';
+export const otherColor = "#bab0ab";
 
 export const colorPalette = [
-    '#4E79A7', // dk blue
-    '#aecBe8', // lt blue
-    '#f28e2b', // orange
-    '#FFBE7D', // lt. orange
-    '#59a14f', // green
-    '#8CD17D', // lt. green
-    '#e15759', // red
-    '#FF9D9A', // lt. red
-    '#499894', // teal
-    '#86BCB6', // lt. teal
-    '#B6992D', // dk yellow
-    '#F1CE63', // yellow
-    '#D37295', // dk pink
-    '#FABFD2', // lt. pink,
-    '#B07AA1', // dk purple
-    '#D4A6C8', // lt. purple
-    '#9D7660', // brown
-    '#D7B5A6', // lt. brown
-    '#bcbd22', // puce
-    '#79706E', // grey
+  "#4E79A7", // dk blue
+  "#aecBe8", // lt blue
+  "#f28e2b", // orange
+  "#FFBE7D", // lt. orange
+  "#59a14f", // green
+  "#8CD17D", // lt. green
+  "#e15759", // red
+  "#FF9D9A", // lt. red
+  "#499894", // teal
+  "#86BCB6", // lt. teal
+  "#B6992D", // dk yellow
+  "#F1CE63", // yellow
+  "#D37295", // dk pink
+  "#FABFD2", // lt. pink,
+  "#B07AA1", // dk purple
+  "#D4A6C8", // lt. purple
+  "#9D7660", // brown
+  "#D7B5A6", // lt. brown
+  "#bcbd22", // puce
+  "#79706E", // grey
 ];
 
+export const ylGnBuDiscrete11 = [
+  "#ffffd9",
+  "#eff9bd",
+  "#d5eeb3",
+  "#a9ddb7",
+  "#73c9bd",
+  "#45b4c2",
+  "#2897bf",
+  "#2073b2",
+  "#234ea0",
+  "#1c3185",
+  "#081d58",
+];
+
+// Diagonal hatch pattern
+export const diagonalHatchPatternDef = (
+  patternId = "diagonalHatch",
+  strokeColor = "#a9a9a9",
+  strokeWidth = 2
+) => `
+  <pattern
+    id="${patternId}"
+    width="5"
+    height="5"
+    patternTransform="rotate(45 0 0)"
+    patternUnits="userSpaceOnUse"
+  >
+    <line
+      x1="0"
+      y1="0"
+      x2="0"
+      y2="10"
+      style="stroke:${strokeColor}; stroke-width:${strokeWidth}"
+    />
+  </pattern>
+`;
