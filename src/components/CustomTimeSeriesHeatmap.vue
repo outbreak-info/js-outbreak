e586bd9b7371d0eca1056a07c71f3bad44d27f0a
Leandro Collares <leco@uvic.ca>
2025-12-15 21:02:30 -0300

    feat: add row labels

--- src/components/CustomTimeSeriesHeatmap.vue
+++ src/components/CustomTimeSeriesHeatmap.vue
@@ -7,13 +7,15 @@ import {
   ylOrRdDiscrete11,
   diagonalHatchPatternDef,
 } from "../utils/colorSchemes";
+import { createDateArray } from "../utils/arrays";
 
 const props = defineProps({
   aggregatedData: { type: Array, required: true },
+  dateRange: { type: Number, default: 30 },
   
   // Property key configuration
-  rowKey: { type: String, default: "rowValue" },
-  columnKey: { type: String, default: "columnValue" },
+  rowKey: { type: String, default: "name" },
+  columnKey: { type: String, default: "collection_date" },
   colorKey: { type: String, default: "colorValue" },
 
    // Color scale configuration
@@ -42,6 +44,7 @@ const props = defineProps({
 });
 
 const width = ref(500);
+const rowHeight = 20;
 
 onMounted(() => {
   window.addEventListener("resize", handleResize);
@@ -78,7 +81,10 @@ const containerMargins = computed(() => ({
   marginLeft: props.containerMarginLeft + "px",
 }));
 
-const containerWidth = ref(500);
+const marginTop = props.marginTop;
+const marginRight = props.marginRight;
+const marginBottom = props.marginBottom;
+const marginLeft = props.marginLeft;
 
 // Legend dimensions
 const legendWidth = 300;
@@ -110,6 +116,44 @@ const colorBands = colorScale.value.range().map((color) => {
 const legendTicks = colorScale.value.domain();
 
 const formatLegendValue = format(".2s");
+
+const rowLabels = computed(() =>
+  [...new Set(props.aggregatedData.map(yAccessor))]
+    .sort((a, b) => {
+      if (a === 'Other') return 1;
+      if (b === 'Other') return -1;
+      return a.localeCompare(b);
+    }),
+);
+
+const minMaxDates = computed(() => {
+  const dates = props.aggregatedData.map(xAccessor).sort();
+  return [dates[0], dates[dates.length - 1]];
+});
+
+const xScaleDomain = computed(() =>
+  createDateArray(minMaxDates.value[0], minMaxDates.value[1], props.dateRange),
+);
+
+
+const height = computed(() => rowLabels.value.length > 2 ? (rowHeight * rowLabels.value.length) + (5 * rowLabels.value.length - 1) : (rowHeight * rowLabels.value.length) + 15);
+
+const innerWidth = computed(() => width.value - marginLeft - marginRight);
+const innerHeight = computed(() => height.value - marginTop - marginBottom);
+
+const xScale = computed(() =>
+  scaleBand()
+    .domain(xScaleDomain.value)
+    .range([0, innerWidth.value])
+    .paddingInner(0.15),
+);
+
+const yScale = computed(() =>
+  scaleBand()
+    .domain(rowLabels.value)
+    .range([0, innerHeight.value])
+    .paddingInner(0),
+);
 
 // Heatmap container inline styles
 const heatmapContainerStyle = computed(() => ({
@@ -195,6 +239,30 @@ const noDataStyle = {
           </text>
         </svg>
       </div>
+    </div>
+    <!-- grid -->
+    <div>
+      <svg
+        role="img"
+        :width="width - containerMarginLeft - containerMarginRight"
+        :height="height"
+      >
+        <g :transform="`translate(${marginLeft}, ${marginTop})`">
+          <g v-for="rowLabel in rowLabels">
+            <text
+              text-anchor="end"
+              x="-10"
+              :y="yScale(rowLabel) + yScale.bandwidth() / 2"
+              dy=".30em"
+              fill="2c3e50"
+              font-size="14px"
+              font-weight="400"
+            >
+              {{ rowLabel }}
+            </text>
+          </g>
+        </g>
+      </svg>
     </div>
   </div>
 </template>
