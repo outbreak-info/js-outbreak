488bd0b6ca03140b6889aa435ad607985dee70df
Leandro Collares <leandrocollares@users.noreply.github.com>
2026-03-15 12:54:57 -0300

    feat: add radio button group (#36)

---
+++ tests/visual/components/TestRadioButtonGroup.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+import RadioButtonGroup from '../../../src/components/RadioButtonGroup.vue';
+
+const lineageOptions = [
+  { label: "WHO variant", value: "whovariant" },
+  { label: "Phylogenetic", value: "phylogenetic" }
+];
+
+const selectedLineageGroupingApproach = ref("whovariant");
+</script>
+
+<template>
+  <div>
+    <h1>Radio Button Group Component Test</h1>
+    <RadioButtonGroup
+      v-model="selectedLineageGroupingApproach"
+      label="Lineage grouping approach"
+      :options="lineageOptions"   
+    />
+    <p>Selected: {{ selectedLineageGroupingApproach }}</p>
+  </div>
+</template>
+
